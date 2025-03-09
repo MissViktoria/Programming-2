2fe16a0c68a411df04f9d429bbda658332e8e366
MissViktoria <victoriasteb55@gmail.com>
2025-03-09 09:39:09 +0700

    Создала файл sort.c с BubbleSort

---
+++ sort.c
@@ -0,0 +1,12 @@
+void BubbleSort(int a[], int n){
+	int i, j, t;
+	for (i=0; i < n-1; i++){
+		for (i=0; j < n-i-1; j++){
+			if (a[j] > a[j+1]){
+				t = a[j];
+				a[j] = a[j+1];
+				a[j+1] = t;
+			}
+		}
+	}
+}
